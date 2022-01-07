a4c6298b64c1a84357ada0a45668a192d44f7f88
Md Maruf Ahmed <maruf.ahmed76@yahoo.com>
2022-01-07 17:48:08 +0600

    Add home content

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,29 @@
 import Head from "next/head";
 import Navbar from "components/Navbar";
 import Slogan from "components/Slogan";
+import Hero from "components/Hero";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Slogan />
+      <Hero />
+      <div className="container mx-auto px-4 py-10">
+        <div className="flex flex-col gap-4">
+          <p className=" italic text-gray-500 text-sm">
+            “Dennis-Benn is a prodigious world-straddler, and not just
+            geographically; her characters are <br /> memorable and fully drawn,
+            and the devastating meta-legacies they conjure are all too real.”
+          </p>
+          <p className="text-center text-deepBlue font-bold text-lg">- VOGUE</p>
+        </div>
+        <div className=" flex justify-center mt-10">
+          <button className=" uppercase bg-gradient-to-r from-deepIndigo to-lightIndigo py-4 px-16 text-white font-bold rounded">
+            Learn more
+          </button>
+        </div>
+      </div>
     </>
   );
 }
