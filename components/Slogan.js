5618629ec358a9c0f3348fb4831a2ceaf88dbec5
Md Maruf Ahmed <maruf.ahmed76@yahoo.com>
2022-01-07 17:54:53 +0600

    Add class bg utils

--- components/Slogan.js
+++ components/Slogan.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Slogan(props) {
   return (
-    <div className="slogan p-4 text-center">
+    <div className=" bg-gradient-to-br from-lightYellow to-deepYellow p-4 text-center">
       <p className="font-bold italic text-2xl text-deepBlue">
         Stories No One Dares To Tell!
       </p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,8 @@ module.exports = {
         deepBlue: "#172952",
         lightIndigo: "#8B00BD",
         deepIndigo: "#4A0065",
+        lightYellow: "#f9c258",
+        deepYellow: "#f8a924",
       },
     },
   },
