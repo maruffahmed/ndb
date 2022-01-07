aca7dbd10d53a7db01cf9af45a6ba92328c0eb44
Md Maruf Ahmed <maruf.ahmed76@yahoo.com>
2022-01-07 17:12:28 +0600

    Add slogan component

---
+++ components/Slogan.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Slogan(props) {
+  return (
+    <div className="slogan p-4 text-center">
+      <p className="font-bold italic text-2xl text-deepBlue">
+        Stories No One Dares To Tell!
+      </p>
+    </div>
+  );
+}
+
+export default Slogan;
