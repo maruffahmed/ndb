09628392bd45ba17340ab3bc0dc81935cc855efb
Md Maruf Ahmed <maruf.ahmed76@yahoo.com>
2022-01-09 20:45:16 +0600

    Fixed books card image background

--- components/BookList/BookCard.js
+++ components/BookList/BookCard.js
@@ -5,8 +5,11 @@ function BookCard({ bookDetails }) {
   const { title, quotes, author, href, thumbnail } = bookDetails;
   return (
     <div className="card">
-      <div className="card-image flex justify-center items-center ">
-        <img src={thumbnail} alt="book" className=" w-5/6" />
+      <div className="flex justify-center items-center relative">
+        <img src={thumbnail} alt="book" className=" w-5/6 z-20" />
+        <div className=" absolute inset-0 z-10">
+          <img src="./imgs/itemBg1.png" alt="bg" className=" h-full w-full" />
+        </div>
       </div>
       <div className=" bg-white">
         <BookDetails
